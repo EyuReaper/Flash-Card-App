d0f0d9a370089a4fa359f8d7da1a5559afff5341
EyuelGetachew_X <32816972+EyuReaper@users.noreply.github.com>
2024-09-04 22:04:54 +0300

    updated READ.me

--- flash/src/index.js
+++ flash/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./src/App.css';
+import ".flash/src/App.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
